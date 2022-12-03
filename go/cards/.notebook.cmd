8e046309511d9f2b8c5b72ea32c3f7e4c23a08bb
DangPham112000 <dangpham112000@gmail.com>
2022-12-03 11:09:06 +0700

    function

---
+++ go/cards/.notebook.cmd
@@ -0,0 +1,10 @@
+# Basic Go types
+// bool string int float64
+
+// We can initialize a variable outside of a function, we just can't assign a value to it.
+
+// 2 syntax are 100% similar
+	var card string = "Ace of Spades"
+	card := "Ace of Spades"
+
+// Files in the same package can freely call functions defined in other files.
---
+++ go/cards/main.go
@@ -0,0 +1,18 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	card := newCard()
+// 	fmt.Print(card)
+// }
+
+// func newCard() string {
+// 	return "Ace of Spades"
+// }
+
+package main
+
+func main() {
+	printState()
+}
