055d3012a707fb3274f5418b83255dce6fafbeec
DangPham112000 <dangpham112000@gmail.com>
2022-12-12 06:46:32 +0700

    test case for saveToFile and newDeckFromFile

--- go/cards/deck_test.go
+++ go/cards/deck_test.go
@@ -1,11 +1,41 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16 but %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card is Ace of Spaces but %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Hearts" {
+		t.Errorf("Expected last card is Four of Hearts but %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.shuffer()
+
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck length 16 but %v", len(loadedDeck))
+	}
+
+	if loadedDeck[0] != deck[0] {
+		t.Errorf("Expected loaded deck is the same old but %v", loadedDeck[0])
 	}
 }
