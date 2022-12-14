c5c7f67cded5a156bc48552ccf38642e41136c3e
DangPham112000 <dangpham112000@gmail.com>
2022-12-14 16:05:01 +0700

    structs with receiver funcs

--- go/structs/main.go
+++ go/structs/main.go
@@ -23,5 +23,14 @@ func main() {
 		},
 	}
 
-	fmt.Printf("%+v", danh)
+	danh.updateName("Danh lol")
+	danh.print()
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
